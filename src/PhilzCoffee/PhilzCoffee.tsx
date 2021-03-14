813a6b37f1ff6c9c7d04bc367cfe96fb295231b6
wonhopark89-dev <wonhopark89.dev@gmail.com>
2021-03-15 00:48:21 +0900

    Add scroll view detail option

--- src/PhilzCoffee/PhilzCoffee.tsx
+++ src/PhilzCoffee/PhilzCoffee.tsx
@@ -8,16 +8,24 @@ import Cards from './components/Cards';
 
 const {width} = Dimensions.get('window');
 
+const snapToOffsets = [0, CARD_HEIGHT];
 const styles = StyleSheet.create({
   slider: {height: CARD_HEIGHT}
 });
 
+// snapToInterval : 스크롤 단위 ? 화면 단위로 스크롤이 될때 주로 width 값
+// snapToEnd : false 이면 한번에 끝으로 안감
 const PhilzCoffee = () => {
   return (
     <View style={{flex: 1}}>
-      <ScrollView bounces={false} showsVerticalScrollIndicator={false}>
+      <ScrollView
+        decelerationRate={'fast'}
+        snapToOffsets={snapToOffsets}
+        bounces={false}
+        showsVerticalScrollIndicator={false}
+        snapToEnd={false}>
         <View style={styles.slider}>
-          <ScrollView horizontal>
+          <ScrollView decelerationRate={'fast'} snapToInterval={width} showsHorizontalScrollIndicator={false} horizontal>
             {products.map((product, index) => (
               <Card product={product} key={index} />
             ))}
