20da1e6c22f6cb8855b20ad4b7aa4af539984e01
wonhopark89-dev <wonhopark89.dev@gmail.com>
2021-03-15 01:41:47 +0900

    Add scroll animation about PhilzCoffee

--- src/PhilzCoffee/PhilzCoffee.tsx
+++ src/PhilzCoffee/PhilzCoffee.tsx
@@ -5,7 +5,7 @@ import {products} from './Model';
 import Card, {CARD_HEIGHT} from './Card';
 import Products from './Products';
 import Cards from './components/Cards';
-
+import Animated, {interpolateColor, useAnimatedScrollHandler, useAnimatedStyle, useSharedValue} from 'react-native-reanimated';
 const {width} = Dimensions.get('window');
 
 const snapToOffsets = [0, CARD_HEIGHT];
@@ -16,8 +16,24 @@ const styles = StyleSheet.create({
 // snapToInterval : 스크롤 단위 ? 화면 단위로 스크롤이 될때 주로 width 값
 // snapToEnd : false 이면 한번에 끝으로 안감
 const PhilzCoffee = () => {
+  const translateX = useSharedValue(0);
+  const onScroll = useAnimatedScrollHandler({
+    onScroll: ({contentOffset: {x}}) => {
+      translateX.value = x;
+    }
+  });
+
+  const style = useAnimatedStyle(() => ({
+    flex: 1,
+    backgroundColor: interpolateColor(
+      translateX.value,
+      products.map((_, i) => width * i), // input range : interpolate 가 이루어지는 크기 단위 라고 생각하면됨
+      products.map((product) => product.color2)
+    )
+  }));
+
   return (
-    <View style={{flex: 1}}>
+    <Animated.View style={style}>
       <ScrollView
         decelerationRate={'fast'}
         snapToOffsets={snapToOffsets}
@@ -25,16 +41,22 @@ const PhilzCoffee = () => {
         showsVerticalScrollIndicator={false}
         snapToEnd={false}>
         <View style={styles.slider}>
-          <ScrollView decelerationRate={'fast'} snapToInterval={width} showsHorizontalScrollIndicator={false} horizontal>
+          <Animated.ScrollView
+            onScroll={onScroll}
+            scrollEventThrottle={16}
+            decelerationRate={'fast'}
+            snapToInterval={width}
+            showsHorizontalScrollIndicator={false}
+            horizontal>
             {products.map((product, index) => (
               <Card product={product} key={index} />
             ))}
-          </ScrollView>
-          <Products />
+          </Animated.ScrollView>
+          <Products x={translateX} />
         </View>
         <Cards />
       </ScrollView>
-    </View>
+    </Animated.View>
   );
 };
 
--- src/PhilzCoffee/Products.tsx
+++ src/PhilzCoffee/Products.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {View, StyleSheet, Image, Dimensions} from 'react-native';
 
 import {products} from './Model';
+import Animated, {interpolate, useAnimatedStyle} from 'react-native-reanimated';
+import {CARD_HEIGHT} from '~/PhilzCoffee/Card';
 
 const {width} = Dimensions.get('window');
 const SIZE = 200;
@@ -10,19 +12,46 @@ const styles = StyleSheet.create({
     ...StyleSheet.absoluteFillObject,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  imageStyle: {
+    ...StyleSheet.absoluteFillObject,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    bottom: CARD_HEIGHT / 5
   }
 });
 
-const Products = () => {
+interface ProductProps {
+  product: typeof products[0];
+  index: number;
+  x: Animated.SharedValue<number>;
+}
+
+const Product = ({product, index, x}: ProductProps) => {
+  const style = useAnimatedStyle(() => {
+    const inputRange = [width * (index - 1), width * index, width * (index + 1)];
+    const translateX = interpolate(x.value, inputRange, [width / 2, 0, -width / 2]);
+    const scale = interpolate(x.value, inputRange, [0.75, 1, 0.75]);
+    return {transform: [{translateX}, {scale}]};
+  });
+
+  return (
+    <Animated.View key={index} style={[styles.imageStyle, style]}>
+      <Image source={product.picture} style={{width: SIZE, height: SIZE * product.aspectRatio}} />
+    </Animated.View>
+  );
+};
+
+interface ProductsProps {
+  x: Animated.SharedValue<number>;
+}
+
+const Products = ({x}: ProductsProps) => {
   return (
     <View style={styles.container} pointerEvents="none">
-      {products.map((product, index) => {
-        return (
-          <View key={index} style={styles.container}>
-            <Image source={product.picture} style={{width: SIZE, height: SIZE * product.aspectRatio}} />
-          </View>
-        );
-      })}
+      {products.map((product, index) => (
+        <Product key={index} product={product} index={index} x={x} />
+      ))}
     </View>
   );
 };
